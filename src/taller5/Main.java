9e7c77c4bba47b17061ea853a4390c4cbdaa06bb
Acer <Acer@LAPTOP-JU3K5HIU>
2026-04-13 17:50:09 -0500

    inicio del proyecto

---
+++ src/taller5/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taller5;
+
+/**
+ *
+ * @author Acer
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Producto p1 = new Producto("Laptop", 2500, 10);
+        p1.mostrarInfo();
+    }
+    
+}
