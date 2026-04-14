4acf4d50e66d91e9a12b7b338cc9dac49261bc2e
Alexander Cervantes <alexandercervantes0413@gmail.com>
2026-04-13 19:35:46 -0500

    feat(taller5): ejercicios completos de modificador default

---
+++ src/otroPaquete/Prueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package otroPaquete;
+
+import taller5.Producto;
+
+public class Prueba {
+    public static void main(String[] args) {
+
+        Producto p = new Producto("Celular", 1200, 5);
+
+        p.mostrarInfo();
+    }
+}
